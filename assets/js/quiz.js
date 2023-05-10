6d53689a43d1ffe7f8776b4fcdabb9e9613a9fc6
Evelyn-H <hobert.evelyn@gmail.com>
2023-05-10 17:53:30 -0500

    throw in some oop

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,19 @@
 // window.onload = () => alert("test")
 
 function build(tag, ...classes) {
-    var el = document.createElement("a");
+    var el = document.createElement(tag);
     el.classList.add(classes);
     return el
 }
 
+class Question {
+    constructor(id, question, answers) {
+        this.id = id;
+        this.question = question;
+        this.answers = answers;
+        // other things: (initial) selection, onclick callback
+    }
+  }
 
 // example output structure of this function:
 /* 
@@ -20,15 +28,14 @@ function build(tag, ...classes) {
     </div>
 </section> 
 */
-function add_question(q, a) {
+function add_question(q) {
     var question_list = document.getElementById("questions");
 
     var question = build("div", "question");
-    question.textContent = q;
+    question.textContent = q.question;
 
     var answers = build("div", "answers");
-    a.forEach(choice => {
-        console.log(choice);
+    q.answers.forEach(choice => {
         var el = build("div", "choice")
         el.textContent = choice;
         answers.append(el);
@@ -37,29 +44,34 @@ function add_question(q, a) {
     var el = document.createElement("section");
     el.append(question);
     el.append(answers);
-    console.log(el);
 
     question_list.append(el);
 }
 
 addEventListener("DOMContentLoaded", () => {
-    add_question("Why did you buy bamboo?", [
-        "for hitting people with", 
-        "for planting",
-        "for attaching to people",
-        "just so I Have Bamboo",
-        "also for hanging people off of",
-    ]);
+    var questions = [
+        new Question(1, "Why did you buy bamboo?", [
+            "for hitting people with", 
+            "for planting",
+            "for attaching to people",
+            "just so I Have Bamboo",
+            "also for hanging people off of",
+        ]),
 
-    add_question("choose one. there's only one correct answer.", [
-        "tautline hitch", 
-        "farrimond friction hitch",
-    ]);
+        new Question(2, "choose one. there's only one correct answer.", [
+            "tautline hitch", 
+            "farrimond friction hitch",
+        ]),
 
-    add_question("“How did you get your name in the climbing server?", [
-        "I picked it myself", 
-        "it matches the 7 other puns in this series",
-        "my profoundly equine qualities",
-        "no idea, this is some random garbage someone thrust upon me",
-    ]);
+        new Question(3, "How did you get your name in the climbing server?", [
+            "I picked it myself", 
+            "it matches the 7 other puns in this series",
+            "my profoundly equine qualities",
+            "no idea, this is some random garbage someone thrust upon me",
+        ]),
+
+    ];
+
+    questions.forEach(add_question);
+
 });
